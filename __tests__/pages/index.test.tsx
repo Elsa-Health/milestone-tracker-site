26ad27a53f46930ca0337bf92654fdf874824538
3210jr <3210jr@gmail.com>
2020-12-20 18:04:25 +0300

    update package.json

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // import react-testing methods
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 // add custom jest matchers from jest-dom
 import "@testing-library/jest-dom/extend-expect";
